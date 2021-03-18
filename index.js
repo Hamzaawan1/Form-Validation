c144f34cd4b167c896416bca7c173dbcc42a2d38
Hamzoo <awanhamza102@gmail.com>
2021-03-18 22:44:36 +0500

    as

---
+++ index.js
@@ -0,0 +1,22 @@
+$(function () {
+    $(".vform").submit(handleFormSubmit);
+  });
+  
+  function handleFormSubmit(e) {
+    if ($(".name").val() <= 5) {
+      $(".name").css({ border: "2px solid red" });
+      $(".pname").text("Name Must be at least 5 characters long");
+    } else if ($(".email").val() == 0) {
+      $(".email").css({ border: "2px solid red" });
+      $(".pemail").text("Please enter valid email address!");
+    } else if ($("#password").val() < 8) {
+      $("#ppassword").css({ border: "2px solid red" });
+      $("#ppasswordconfirm").text("Password Must be at least 8 characters long");
+    } else if ($("#password").val() != $("#confirmpassword").val()) {
+      $("#password").css({ border: "2px solid red" });
+      $("#ppasswordconfirm").text("Password not Matched");
+    } else {
+      $("#psubmit").text("Form is being submitted");
+    }
+    e.preventDefault();
+  }
